53fba505a2d78096bcc891654e057b55b3adc2b6
Devaj Sanghvi <devajsanghvi15@gmail.com>
2024-05-22 03:21:16 +0530

    settings.py not changed

--- crud/settings.py
+++ crud/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# https://docs.djangoproject.com/en/5.0/howto/static-files/\
+
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
